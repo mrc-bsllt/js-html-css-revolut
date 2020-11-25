ebeee756be48671e20c3822960fdf58055805e47
MarcoB <mrc.bsllt@gmail.com>
2020-11-25 16:47:35 +0100

    completata la versione con il click

--- js/script.js
+++ js/script.js
@@ -9,26 +9,38 @@ $(document).ready(
     var liPositionRelative = $("li.position_relative");
     // VERSIONE CON IL MOUSE ALL'HOVER--------------------------------
 
-    // quando vado sopra con il mouse alla li che contiene il dropdown menu
-    liPositionRelative.mouseenter(
-      function() {
-        $(this).children("a").css("color", "#191c1f");
-        var indexLi = $(this).index();
-        $(dropdownUl[indexLi]).fadeIn("fast");
-      }
-    );
-
-    // quando sposto il mouse dalla li che contiene il dropdown menu
-    liPositionRelative.mouseleave(
-      function() {
-        $(this).children("a").css("color", "#b0b1b2");
-        var indexLi = $(this).index();
-        $(dropdownUl[indexLi]).fadeOut(200);
-      }
-    );
+    // // quando vado sopra con il mouse alla li che contiene il dropdown menu
+    // liPositionRelative.mouseenter(
+    //   function() {
+    //     $(this).children("a").css("color", "#191c1f");
+    //     var indexLi = $(this).index();
+    //     $(dropdownUl[indexLi]).fadeIn("fast");
+    //   }
+    // );
+    //
+    // // quando sposto il mouse dalla li che contiene il dropdown menu
+    // liPositionRelative.mouseleave(
+    //   function() {
+    //     $(this).children("a").css("color", "#b0b1b2");
+    //     var indexLi = $(this).index();
+    //     $(dropdownUl[indexLi]).fadeOut(200);
+    //   }
+    // );
 
     // VERSIONE CON IL CLICK--------------------------------
 
+    liPositionRelative.click(
+      function() {
+        var indexLi = $(this).index();
+        $(dropdownUl[indexLi]).fadeToggle();
+      }
+    );
+
+    liPositionRelative.mouseleave(
+      function() {
+        dropdownUl.fadeOut(200);
+      }
+    );
 
   }
 );
