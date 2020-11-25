870243e4700b3960a9c43eec41e7f51b5b305f60
MarcoB <mrc.bsllt@gmail.com>
2020-11-25 17:37:51 +0100

    bonus in lavorazione, mancano tutte le ul

--- js/script.js
+++ js/script.js
@@ -4,43 +4,43 @@ $(document).ready(
     // le <a> che hanno la classe active devono avere questo colore sempre
     $("a.active").css("color", "#191c1f");
     // tutti gli ul che sono dei dropdown
-    var dropdownUl = $("ul.dropdown_item");
+    var dropdownItem = $(".dropdown_item");
     // tutti gli li che hanno classe position_relative
     var liPositionRelative = $("li.position_relative");
     // VERSIONE CON IL MOUSE ALL'HOVER--------------------------------
 
-    // // quando vado sopra con il mouse alla li che contiene il dropdown menu
-    // liPositionRelative.mouseenter(
-    //   function() {
-    //     $(this).children("a").css("color", "#191c1f");
-    //     var indexLi = $(this).index();
-    //     $(dropdownUl[indexLi]).fadeIn("fast");
-    //   }
-    // );
-    //
-    // // quando sposto il mouse dalla li che contiene il dropdown menu
-    // liPositionRelative.mouseleave(
-    //   function() {
-    //     $(this).children("a").css("color", "#b0b1b2");
-    //     var indexLi = $(this).index();
-    //     $(dropdownUl[indexLi]).fadeOut(200);
-    //   }
-    // );
+    // quando vado sopra con il mouse alla li che contiene il dropdown menu
+    liPositionRelative.mouseenter(
+      function() {
+        $(this).children("a").css("color", "#191c1f");
+        var indexLi = $(this).index();
+        $(dropdownItem[indexLi]).fadeIn("fast");
+      }
+    );
+
+    // quando sposto il mouse dalla li che contiene il dropdown menu
+    liPositionRelative.mouseleave(
+      function() {
+        $(this).children("a").css("color", "#b0b1b2");
+        var indexLi = $(this).index();
+        $(dropdownItem[indexLi]).fadeOut(200);
+      }
+    );
 
     // VERSIONE CON IL CLICK--------------------------------
 
-    liPositionRelative.click(
-      function() {
-        var indexLi = $(this).index();
-        $(dropdownUl[indexLi]).fadeToggle();
-      }
-    );
-
-    liPositionRelative.mouseleave(
-      function() {
-        dropdownUl.fadeOut(200);
-      }
-    );
+    // liPositionRelative.click(
+    //   function() {
+    //     var indexLi = $(this).index();
+    //     $(dropdownItem[indexLi]).fadeToggle();
+    //   }
+    // );
+    //
+    // liPositionRelative.mouseleave(
+    //   function() {
+    //     dropdownItem.fadeOut(200);
+    //   }
+    // );
 
   }
 );
