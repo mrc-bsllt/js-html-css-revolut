35abd034e642a06a391608aff71f2b447f35ff64
MarcoB <mrc.bsllt@gmail.com>
2020-11-25 15:41:38 +0100

    gestito l'hover delle a con jQuery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(
   function() {
+
+    // quando vado in hover sugli a, devono cambiare colore e non avere il text decoration
+    $("a").mouseenter(
+      function() {
+        $(this).css({
+          'color': '#191c1f',
+          'textDecoration': 'none'
+        });
+      }
+    );
+
+    $("a").mouseleave(
+      function() {
+        $(this).css("color", "#b0b1b2");
+      }
+    );
+
+
   }
 );
