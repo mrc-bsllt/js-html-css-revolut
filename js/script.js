c2530dccac69e4e6d6b39b420008aeb93189a23e
MarcoB <mrc.bsllt@gmail.com>
2020-11-25 16:39:24 +0100

    ordinato il codice js e impostato per fare la versione al click

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ $(document).ready(
 
     // le <a> che hanno la classe active devono avere questo colore sempre
     $("a.active").css("color", "#191c1f");
-
     // tutti gli ul che sono dei dropdown
     var dropdownUl = $("ul.dropdown_item");
+    // tutti gli li che hanno classe position_relative
+    var liPositionRelative = $("li.position_relative");
+    // VERSIONE CON IL MOUSE ALL'HOVER--------------------------------
 
     // quando vado sopra con il mouse alla li che contiene il dropdown menu
-    $("li.position_relative").mouseenter(
+    liPositionRelative.mouseenter(
       function() {
         $(this).children("a").css("color", "#191c1f");
         var indexLi = $(this).index();
@@ -17,13 +19,16 @@ $(document).ready(
     );
 
     // quando sposto il mouse dalla li che contiene il dropdown menu
-    $("li.position_relative").mouseleave(
+    liPositionRelative.mouseleave(
       function() {
         $(this).children("a").css("color", "#b0b1b2");
         var indexLi = $(this).index();
         $(dropdownUl[indexLi]).fadeOut(200);
       }
     );
+
+    // VERSIONE CON IL CLICK--------------------------------
+
 
   }
 );
